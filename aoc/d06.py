fcd92f52f3d651ddec46e71dea0952476dec872a
lakret <Rakuretu@gmail.com>
2024-12-07 15:57:35 +0100

    d06 cleanup

--- aoc/d06.py
+++ aoc/d06.py
@@ -1,5 +1,4 @@
 from aoc.helpers import *
-from collections import defaultdict
 from dataclasses import dataclass, replace
 
 
@@ -15,6 +14,7 @@ class State:
     max_col: int
 
     def walk(self) -> "State":
+        """Moves one step ahead or does one turn, if necessary."""
         new_pos = self.pos[0] + self.direction[0], self.pos[1] + self.direction[1]
         if new_pos in self.obstacles:
             return replace(self, direction=TURNS[self.direction])
@@ -22,7 +22,7 @@ class State:
             return replace(self, pos=new_pos)
 
     def walk_to_exit(self) -> set[tuple[int, int]]:
-        """Returns a set of visited positions"""
+        """Returns a set of positions visited before the guard exits the map."""
         visited, state = set(), replace(self)
         while state.inside():
             visited.add(state.pos)
@@ -32,30 +32,30 @@ class State:
     def inside(self) -> bool:
         return self.pos[0] <= self.max_row and self.pos[1] <= self.max_col and self.pos[0] >= 0 and self.pos[1] >= 0
 
+    def parse(input: str) -> "State":
+        obstacles, pos, direction, max_row, max_col = set(), None, None, 0, 0
 
-def parse(input: str) -> State:
-    obstacles, pos, direction, max_row, max_col = set(), None, None, 0, 0
-    for row_idx, row in enumerate(input.splitlines()):
-        for col_idx, ch in enumerate(row):
-            match ch:
-                case ".":
-                    continue
-                case "#":
-                    obstacles.add((row_idx, col_idx))
-                case _:
-                    pos = (row_idx, col_idx)
-                    match ch:
-                        case "^":
-                            direction = (-1, 0)
-                        case "v":
-                            direction = (1, 0)
-                        case ">":
-                            direction = (0, 1)
-                        case "<":
-                            direction = (0, -1)
-            max_row = max(max_row, row_idx)
-            max_col = max(max_col, col_idx)
-    return State(obstacles=obstacles, pos=pos, direction=direction, max_row=max_row, max_col=max_col)
+        for row_idx, row in enumerate(input.splitlines()):
+            for col_idx, ch in enumerate(row):
+                match ch:
+                    case ".":
+                        continue
+                    case "#":
+                        obstacles.add((row_idx, col_idx))
+                    case _:
+                        pos = (row_idx, col_idx)
+                        match ch:
+                            case "^":
+                                direction = (-1, 0)
+                            case "v":
+                                direction = (1, 0)
+                            case ">":
+                                direction = (0, 1)
+                            case "<":
+                                direction = (0, -1)
+                max_row = max(max_row, row_idx)
+                max_col = max(max_col, col_idx)
+        return State(obstacles=obstacles, pos=pos, direction=direction, max_row=max_row, max_col=max_col)
 
 
 def p1(state: State) -> int:
@@ -63,33 +63,37 @@ def p1(state: State) -> int:
 
 
 def is_loop(state: State) -> bool:
-    # we need to track guard's direction this time to detect loops, and we need to support guard
-    # visiting the same cell while walking in different directions, thus we're using set for values
-    visited = defaultdict(set)
-    visited[state.pos] = {state.direction}
+    """
+    A path is a loop when we encounter the guard going in the same direction on the same cell.
+    """
+    visited = set()
     while state.inside():
         state = state.walk()
-        # we arrived at the same point with the same direction for the second time - cycle found
-        if visited.get(state.pos) and state.direction in visited[state.pos]:
+        if (state.pos, state.direction) in visited:
             return True
         else:
-            visited[state.pos].add(state.direction)
+            visited.add((state.pos, state.direction))
+
     return False
 
 
-def p2(state: State) -> int:
-    # it'll take to long to check all possible position, but we have a natural optimization:
-    # just check placing obstacles on the guard's original path only
-    states = [
+def obstacle_placements(state: State) -> list[State]:
+    """
+    The guard can be diverted into a loop only if an obstacle is placed on her original path.
+    """
+    return [
         replace(state, obstacles={obstacle_pos, *state.obstacles})
         for obstacle_pos in state.walk_to_exit()
         if obstacle_pos not in state.obstacles and obstacle_pos != state.pos
     ]
-    return len(list(filter(is_loop, states)))
+
+
+def p2(state: State) -> int:
+    return len(list(filter(is_loop, obstacle_placements(state))))
 
 
 if __name__ == "__main__":
-    input = read_input("d06", parser=parse)
+    input = read_input("d06", parser=State.parse)
 
     p1_ans = p1(input)
     assert p1_ans == 4665
@@ -101,16 +105,16 @@ if __name__ == "__main__":
 
 
 def test_p1():
-    input = read_test_input("d06", parser=parse)
+    input = read_test_input("d06", parser=State.parse)
     assert p1(input) == 41
 
 
 def test_p2():
-    input = read_test_input("d06", parser=parse)
+    input = read_test_input("d06", parser=State.parse)
     assert p2(input) == 6
 
 
 def test_is_loop_detects_loops_in_all_test_positions():
-    input = read_test_input("d06", parser=parse)
+    input = read_test_input("d06", parser=State.parse)
     pos = [(6, 3), (7, 6), (7, 7), (8, 1), (8, 3), (9, 7)]
     assert all([is_loop(replace(input, obstacles={new_obstacle_pos, *input.obstacles})) for new_obstacle_pos in pos])
