d0ee1527af90efeed256139e37009e6181d88c38
lakret <Rakuretu@gmail.com>
2024-12-08 17:38:20 +0100

    shorter d08 solution

--- aoc/d08.py
+++ aoc/d08.py
@@ -1,4 +1,5 @@
 from aoc.helpers import *
+from typing import Optional
 from dataclasses import dataclass
 from collections import defaultdict
 from itertools import combinations
@@ -22,36 +23,26 @@ class Map:
             case antennas:
                 res = set()
                 for a1, a2 in combinations(antennas, 2):
-                    res = res.union(
-                        self._antinodes_for_pair_part2(a1, a2) if part2 else self._antinodes_for_pair(a1, a2)
-                    )
+                    res = res.union(self._antinodes_for_pair(a1, a2, max_k=None if part2 else 1))
                 return res
-
-    def _antinodes_for_pair(self, a1: tuple[int, int], a2: tuple[int, int]) -> set[tuple[int, int]]:
-        drow, dcol = a2[0] - a1[0], a2[1] - a1[1]
-        candidates = [(a1[0] - drow, a1[1] - dcol), (a2[0] + drow, a2[1] + dcol)]
-        return {c for c in candidates if self.is_inside(c)}
 
     def is_inside(self, c: tuple[int, int]) -> bool:
         return c[0] >= 0 and c[0] <= self.max_row and c[1] >= 0 and c[1] <= self.max_col
 
-    def _antinodes_for_pair_part2(self, a1: tuple[int, int], a2: tuple[int, int]) -> set[tuple[int, int]]:
-        res, k, drow, dcol = {a1, a2}, 1, a2[0] - a1[0], a2[1] - a1[1]
-        while True:
-            node = (a1[0] - k * drow, a1[1] - k * dcol)
-            if self.is_inside(node):
-                res.add(node)
-                k += 1
-            else:
-                break
-        k = 0
-        while True:
-            node = (a2[0] + k * drow, a2[1] + k * dcol)
-            if self.is_inside(node):
-                res.add(node)
-                k += 1
-            else:
-                break
+    def _antinodes_for_pair(
+        self, a1: tuple[int, int], a2: tuple[int, int], max_k: Optional[int] = None
+    ) -> set[tuple[int, int]]:
+        res = {a1, a2} if max_k is None else set()
+        drow, dcol = a2[0] - a1[0], a2[1] - a1[1]
+        for origin, direction in [(a1, -1), (a2, 1)]:
+            k = 1
+            while max_k is None or k <= max_k:
+                node = (origin[0] + direction * k * drow, origin[1] + direction * k * dcol)
+                if self.is_inside(node):
+                    res.add(node)
+                    k += 1
+                else:
+                    break
         return res
 
 
